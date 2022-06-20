217d3b9f24051a96a22014ba153a6e760e7dffa4
toremann <50668920+toremann@users.noreply.github.com>
2022-06-20 22:59:21 +0200

    scraper with next.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
-export default function Home() {
+const defaultEndpoint =
+  "https://www.finn.no/api/search-qf?searchkey=SEARCH_ID_BAP_COMMON&abTestKey=controlsuggestions&q=lego&sort=PUBLISHED_DESC&page=1&include_filters=false";
+
+export async function getServerSideProps({ req, res }) {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+
+  const r = await fetch(defaultEndpoint);
+
+  const data = await r.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Home({ data }) {
+  const { results = [] } = data.docs;
+
+  const MINUTE_MS = 60000;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Reloading");
+      location.reload();
+    }, MINUTE_MS);
+
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +46,31 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h3 className={styles.title}>Lego bot</h3>
+        <table>
+          <tbody>
+            {data.docs.map((data, id, docs) => {
+              return (
+                <tr key={id}>
+                  <td>
+                    {" "}
+                    <a
+                      href={`https://www.finn.no/bap/forsale/ad.html?finnkode=${data.ad_id}`}
+                      target="_blank"
+                    >
+                      {data.heading}{" "}
+                    </a>
+                  </td>
+                  <td>
+                    {JSON.stringify(data.price.amount)}{" "}
+                    {JSON.stringify(data.price.currency_code)}
+                  </td>
+                  <td>{new Date(data.timestamp).toLocaleString("en-GB")}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +79,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
