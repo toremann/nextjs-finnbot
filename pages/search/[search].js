37bfb26e95f93ea8474b16fdf7c096c5d2e8435a
toremann <50668920+toremann@users.noreply.github.com>
2022-06-27 14:59:50 +0200

    replace with next router

--- pages/search/[search].js
+++ pages/search/[search].js
@@ -11,7 +11,7 @@ function SearchResults({ results, search, serverTime }) {
   useEffect(() => {
     const interval = setInterval(() => {
       console.log("Reloading");
-      location.reload();
+      router.reload();
     }, MINUTE_MS);
 
     return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
