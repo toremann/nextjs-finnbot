0d9739666ce7abf5046e487f25958afe338b2b00
toremann <50668920+toremann@users.noreply.github.com>
2022-06-27 16:46:47 +0200

    deployment fix

--- pages/search/[search].js
+++ pages/search/[search].js
@@ -3,12 +3,14 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Countdown } from "../../components/Countdown";
 import { Clock } from "./../../components/Clock";
+import Head from "next/head";
 
 function SearchResults({ results, search, serverTime }) {
   const router = useRouter();
   const MINUTE_MS = 60000;
 
   useEffect(() => {
+    const router = useRouter();
     const interval = setInterval(() => {
       console.log("Reloading");
       router.reload();
